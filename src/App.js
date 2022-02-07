8bc0e87128b47da4bbad2d784739b647797a58e8
suneel93 <suneel@msewa.com>
2022-02-07 21:13:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import RegisterForm from "./registration";
+import Loginpage from "./Loginpage";
+function App() {
+  return (
+    <div className="App">
+      <RegisterForm />
+      <Loginpage />
+    </div>
+  );
+}
+
+export default App;
